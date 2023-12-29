a8439e0c9f92d363a9efedf5128bd9193e411704
jumongon <swiftstyles23@gmail.com>
2023-12-29 12:27:17 +0200

    selected language is now stored, fixing a switch issue. small loc fix.

--- src/components/LanguageContext.js
+++ src/components/LanguageContext.js
@@ -1,5 +1,6 @@
-import React, { createContext, useState, useContext } from "react";
+import React, {createContext, useState, useContext, useEffect} from "react";
 import i18n from "../locales/i18n";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LanguageContext = createContext();
 
@@ -8,12 +9,39 @@ export const useLanguage = () => useContext(LanguageContext);
 export const LanguageProvider = ({ children }) => {
     const [language, setLanguage] = useState(i18n.language);
 
+
+    const storeLanguage = async (newLanguage) => {
+        try {
+            await AsyncStorage.setItem('userLanguage', newLanguage);
+            setLanguage(newLanguage)
+            i18n.changeLanguage(newLanguage);
+        } catch (error) {
+            console.error('Failed to save the language to AsyncStorage');
+        }
+    };
+
+
     const toggleLanguage = () => {
         const newLanguage = language === 'en' ? 'ro' : 'en';
-        setLanguage(newLanguage);
-        i18n.changeLanguage(newLanguage);
-        setLanguage(newLanguage);
+        storeLanguage(newLanguage)
     };
+
+
+    useEffect(() => {
+        const loadLanguage = async () => {
+            try {
+                const storedLanguage = await AsyncStorage.getItem('userLanguage');
+                if (storedLanguage !== null) {
+                    setLanguage(storedLanguage);
+                    i18n.changeLanguage(storedLanguage);
+                }
+            } catch (error) {
+                console.error('Failed to retrieve the language from storage')
+            }
+        };
+
+        loadLanguage();
+    }, []);
 
     return (
         <LanguageContext.Provider value={{ language, toggleLanguage}}>
