b5169083991c391c98258c11fbe5174443ce22de
A Cajvan <swiftstyles23@gmail.com>
2024-01-08 22:43:10 +0200

    partial implementation of the checkToken method - more server work needed to proper handle multiple reopen

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import viewTicketsScreen from "./src/screens/ViewTicketsScreen";
 import Register from "./src/screens/Register";
 import Login from "./src/screens/Login";
 import Settings from "./src/screens/Settings";
+import LoadingScreen from './src/components/LoadingScreen';
 import { LanguageProvider } from "./src/components/LanguageContext";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -25,42 +26,54 @@ const DarkTheme = {
 const App = () => {
 
     const [initialRoute, setInitialRoute] = useState('Register');
+    const [isLoading, setIsLoading] = useState(true);
+
     
     useEffect(() => {
         const checkToken = async () => {
             try {
                 const refreshToken = await AsyncStorage.getItem('refreshToken');
+                console.log("Refresh Token from Storage:", refreshToken);
                 if (refreshToken) {
                     const response = await fetch('http://192.168.0.37:3600/auth/refresh', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify({
-                            refreshToken
-                        })
+                        body: JSON.stringify({ refreshToken })
                     });
                     const data = await response.json();
+                    console.log("Response from Refresh:", data);
+    
                     if (data.accessToken) {
                         await AsyncStorage.setItem('accessToken', data.accessToken);
+                        console.log("Setting initial route to Main");
                         setInitialRoute('Main');
                     } else {
+                        console.log("Setting initial route to Login");
                         setInitialRoute('Login');
                     }
+                } else {
+                    console.log("No refresh token, setting initial route to Register");
+                    setInitialRoute('Register');
                 }
             } catch (error) {
                 console.error("Token refresh failed: ", error);
                 setInitialRoute('Login');
-        } 
-    }
+            }
+            setIsLoading(false);
+        };
         checkToken();
-    
     }, []);
+
+    if (isLoading) {
+        return <LoadingScreen />
+    }
 
   return (
       <LanguageProvider>
           <NavigationContainer theme={DarkTheme}>
-            <Stack.Navigator screenOptions={{headerShown: false, animation: "slide_from_right" }} initialRouteName="Register">
+            <Stack.Navigator screenOptions={{headerShown: false, animation: "slide_from_right" }} initialRouteName={initialRoute}>
                 <Stack.Screen name="Register" component={Register} />
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Main" component={MainScreen} />
---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const LoadingScreen = () => {
+  return (
+    <Text>Loading...</Text>
+  )
+}
+
+export default LoadingScreen
